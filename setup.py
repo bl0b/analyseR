2cb4aeded518061ff76df864114db050ec1da0a1
Damien Leroux <damien.leroux@gmail.com>
2012-06-17 18:22:41 +0200

    Added setup script.

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='analyseR',
+      version='0.3',
+      description='Parsing and static code analysis tool for R scripts.',
+      author='Damien "bl0b" Leroux',
+      author_email='damien.leroux@gmail.com',
+      url='http://bl0b.github.com/analyseR/',
+      packages=['analyseR'],
+     )
